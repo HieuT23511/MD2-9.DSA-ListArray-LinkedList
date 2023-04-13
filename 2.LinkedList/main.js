84a3352ed35886d5ba554cf0513accef1b9b40a7
HieuT23511 <hieut23511@gmail.com>
2023-04-13 15:28:29 +0700

    LinkedList Simple

---
+++ 2.LinkedList/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var LinkedList_1 = require("./LinkedList");
+var linkList = new LinkedList_1.LinkedList();
+// console.log(linkList.size);
+linkList.insertFirstNode(50);
